1a84ac5b4bc486a355c239cd11346fd00fd18843
mhdajmal00 <85215783+mhdajmal00@users.noreply.github.com>
2021-06-04 10:34:09 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+
 float wallis_pi(int);
+
 float wallis_pi(int count)
 {
 float value=1.0;
@@ -13,6 +15,7 @@ for(int a=1;a<=count;a++)
  }
 return value*2;
 }
+
 int main(void) {
   float pi;
   for (int i=0; i<5; i++) {
@@ -22,6 +25,7 @@ int main(void) {
       abort();
     }
   }
+
   for (int i=500; i<3000; i++) {
     pi = wallis_pi(i);
     if (!(fabs(pi - M_PI) < 0.01)) {
